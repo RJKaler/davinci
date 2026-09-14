1b2e466cd29ee41bb8f2db95832d021c7d87d93c
R.J. Kaler <31756802+RJKaler@users.noreply.github.com>
2026-09-14 01:25:14 -0400

    Fix error handling in package installation command
    Change the error handling for package installation to use '||' instead of '|'.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ error() { echo "error" && exit 1; }
 
 #trial and error code for missing packages.... 
 sudo apt-get update
-sudo apt-get install fakeroot xorriso ocl-icd-opencl-dev libglu1-mesa libssl3 qtwayland5 -y | error
+sudo apt-get install fakeroot xorriso ocl-icd-opencl-dev libglu1-mesa libssl3 qtwayland5 -y || error
 
 sudo apt install git make -y || error 
 
